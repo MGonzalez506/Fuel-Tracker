ee266c389f20690c76755bcdd84a9da868155dab
Miguel Gonzalez <MGonzalez506@users.noreply.github.com>
2022-06-10 13:25:37 -0600

    Initial Commit

---
+++ Shared/Fuel_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fuel_TrackerApp.swift
+//  Shared
+//
+//  Created by MIGUEL GONZALEZ on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Fuel_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
